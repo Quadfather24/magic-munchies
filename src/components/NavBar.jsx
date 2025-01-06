3d5d9f78ccf79a377263a40b211b928a72e33cea
Brian Zavala <brian.zavala25@proton.me>
2025-01-06 00:53:59 -0600

    Updated mobile and background color to match Nav logo 	new file:   src/assets/logo.png 	new file:   src/assets/vertical-logo.png 	modified:   src/components/NavBar.jsx 	modified:   src/index.css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,12 +3,12 @@ import {
   DisclosureButton,
   DisclosurePanel,
   Menu,
-  MenuButton,
   MenuItem,
   MenuItems,
 } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import { Link } from "react-router-dom";
+import Logo from "../assets/vertical-logo.png";
 
 const navigation = [
   { name: "Home", to: "/", current: true },
@@ -45,11 +45,7 @@ export default function NavBar() {
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             {/* LOGO */}
             <div className="flex shrink-0 items-center">
-              <img
-                alt="Your Company"
-                src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500"
-                className="h-8 w-auto"
-              />
+              <img alt="Your Company" src={Logo} className="h-40 w-auto" />
             </div>
             {/* DESKTOP NAV */}
             <div className="hidden sm:ml-6 sm:block">
@@ -75,36 +71,8 @@ export default function NavBar() {
 
           {/* RIGHT SIDE ICONS (NOTIFICATIONS, PROFILE, ETC.) */}
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <button
-              type="button"
-              className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white 
-                         focus:outline-none focus:ring-2 focus:ring-white 
-                         focus:ring-offset-2 focus:ring-offset-gray-800"
-            >
-              <span className="absolute -inset-1.5" />
-              <span className="sr-only">View notifications</span>
-              <BellIcon aria-hidden="true" className="h-6 w-6" />
-            </button>
-
             {/* Profile dropdown */}
             <Menu as="div" className="relative ml-3">
-              <div>
-                <MenuButton
-                  className="relative flex rounded-full bg-gray-800 text-sm 
-                                      focus:outline-none focus:ring-2 focus:ring-white 
-                                      focus:ring-offset-2 focus:ring-offset-gray-800"
-                >
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">Open user menu</span>
-                  <img
-                    alt=""
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e
-                          ?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9
-                          &auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    className="h-8 w-8 rounded-full"
-                  />
-                </MenuButton>
-              </div>
               <MenuItems
                 transition
                 className="absolute right-0 z-10 mt-2 w-48 origin-top-right 
