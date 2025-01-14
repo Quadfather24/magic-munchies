85b65b44c6650117c679c862fbe40a8c7a4afb99
Brian Zavala <brian.zavala25@proton.me>
2025-01-14 15:04:25 -0600

    optimized contact loading  images.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,30 +5,27 @@ import contact from "../assets/images/background/contact.svg";
 
 function ContactPage() {
   useEffect(() => {
-    // Use more performant options for the observer
+    // Create the observer with options for smoother animations
     const observer = new IntersectionObserver(
       (entries) => {
         entries.forEach((entry) => {
+          // When an element enters the viewport
           if (entry.isIntersecting) {
-            // Add will-change to optimize animations
-            entry.target.style.willChange = "transform, opacity";
             entry.target.classList.add("show");
-            // Remove will-change after animation
-            setTimeout(() => {
-              entry.target.style.willChange = "auto";
-            }, 500);
           } else {
+            // Remove the 'show' class when the element leaves the viewport
             entry.target.classList.remove("show");
           }
         });
       },
       {
         root: null,
-        threshold: 0.1,
-        rootMargin: "50px", // Preload elements before they're visible
+        threshold: 0, // Trigger when just 10% is visible
+        rootMargin: "0px",
       }
     );
 
+    // Get all elements with the 'slide-element' class
     const slideElements = document.querySelectorAll(".slide-element");
     slideElements.forEach((el) => observer.observe(el));
 
@@ -37,7 +34,7 @@ function ContactPage() {
 
   return (
     <div
-      className="min-h-screen w-full h-full mx-auto bg-cover bg-center bg-no-repeat overflow-x-hidden"
+      className="min-h-screen  w-full h-full mx-auto bg-cover bg-center bg-no-repeat overflow-x-hidden"
       style={{
         backgroundImage: `url(${contact1})`,
       }}
@@ -53,22 +50,22 @@ function ContactPage() {
           <h1 className="text-5xl font-extrabold text-transparent bg-clip-text mb-4">
             Let's Connect
           </h1>
-          <p className="text-black text-xl animate__animated animate__fadeInDown animate__faster">
+          <p className="text-black text-xl animate__animated animate__fadeInDown ">
             Scroll down
           </p>
-          <div className="animate__animated animate__fadeOutDown animate__infinite animate__faster">
+          <div className="animate__animated animate__fadeOutDown animate__infinite animate__slow">
             <span className="text-black text-4xl">↓</span>
           </div>
         </div>
       </div>
 
-      <div className="relative z-20 px-4 py-12 space-y-24">
+      <div className="relative z-20 px-4 py-12 space-y-32">
         {/* Facebook Section */}
-        <div className="slide-element slide-left max-w-4xl mx-auto opacity-0 -translate-x-full transition-all duration-500">
-          <div className="bg-gradient-to-br from-blue-500/20 to-blue-600/20 p-8 rounded-2xl shadow-xl hover:scale-105 transition-transform duration-200">
+        <div className="slide-element slide-left max-w-4xl mx-auto opacity-0 -translate-x-full transition-all duration-1000">
+          <div className="bg-gradient-to-br from-blue-500/20 to-blue-600/20  p-8 rounded-2xl shadow-xl hover:scale-105 transition-transform duration-300 hover:ring-2 hover:ring-white">
             <div className="flex flex-col items-center space-y-4">
               <div className="text-blue-600 text-5xl">
-                <i className="fab fa-facebook animate__animated animate__bounce animate__infinite animate__faster"></i>
+                <i className="fab fa-facebook animate__animated animate__bounce animate__infinite"></i>
               </div>
               <h2 className="text-2xl font-bold text-white">
                 Connect on Facebook
@@ -76,9 +73,10 @@ function ContactPage() {
               <p className="text-white text-lg max-w-md text-center">
                 Join our community for updates and special offers!
               </p>
+
               <a
-                href="https://www.facebook.com/groups/265346302113024/"
-                className="mt-2 px-6 py-2 bg-blue-600 text-white rounded-full font-semibold hover:bg-blue-700 transition-colors duration-200"
+                href="https://www.facebook.com/groups/265346302113024/?ref=share&mibextid=NSMWBT"
+                className="mt-2 px-6 py-2 bg-blue-600 text-white rounded-full font-semibold hover:bg-blue-700 transition-colors duration-300 hover:ring-2 hover:ring-white"
               >
                 Visit Our Page
               </a>
@@ -87,11 +85,11 @@ function ContactPage() {
         </div>
 
         {/* Email Section */}
-        <div className="slide-element slide-right max-w-4xl mx-auto opacity-0 translate-x-full transition-all duration-500">
-          <div className="bg-gradient-to-br from-magicTeal/20 to-slate-800/20 backdrop-blur-lg p-8 rounded-2xl shadow-xl hover:scale-105 transition-transform duration-200">
+        <div className="slide-element slide-right max-w-4xl mx-auto opacity-0 translate-x-full transition-all duration-1000">
+          <div className="bg-gradient-to-br from-magicTeal/20 to-slate-800/20 backdrop-blur-lg p-8 rounded-2xl shadow-xl hover:scale-105 transition-transform duration-300 hover:ring-2 hover:ring-black">
             <div className="flex flex-col items-center space-y-4">
               <div className="text-white text-5xl">
-                <i className="far fa-envelope animate__animated animate__bounce animate__infinite animate__faster"></i>
+                <i className="far fa-envelope animate__animated animate__bounce animate__infinite"></i>
               </div>
               <h2 className="text-2xl font-bold text-white">Email Us</h2>
               <p className="text-white text-lg max-w-md text-center">
@@ -99,7 +97,7 @@ function ContactPage() {
               </p>
               <a
                 href="mailto:claudiastreats21@gmail.com"
-                className="mt-2 px-6 py-2 bg text-white rounded-full font-semibold bg-zinc-600 hover:text-black hover:bg-magicTeal transition-colors duration-200"
+                className="mt-2 px-6 py-2 bg text-white rounded-full font-semibold bg-zinc-600 hover:text-black hover:bg-magicTeal hover:ring-2 hover:ring-white transition-colors duration-300"
               >
                 claudiastreats21@gmail.com
               </a>
@@ -108,11 +106,11 @@ function ContactPage() {
         </div>
 
         {/* Phone Section */}
-        <div className="slide-element slide-left max-w-4xl mx-auto opacity-0 -translate-x-full transition-all duration-500">
-          <div className="bg-gradient-to-br from-green-500/20 to-emerald-500/20 backdrop-blur-lg p-8 rounded-2xl shadow-xl hover:scale-105 transition-transform duration-200">
+        <div className="slide-element slide-left max-w-4xl mx-auto opacity-0 -translate-x-full transition-all duration-1000">
+          <div className="bg-gradient-to-br from-green-500/20 to-emerald-500/20 backdrop-blur-lg p-8 rounded-2xl shadow-xl hover:scale-105 transition-transform duration-300 hover:ring-1 hover:ring-white">
             <div className="flex flex-col items-center space-y-4">
               <div className="text-green-600 text-5xl">
-                <i className="fas fa-phone animate__animated animate__bounce animate__infinite animate__faster"></i>
+                <i className="fas fa-phone animate__animated animate__bounce animate__infinite"></i>
               </div>
               <h2 className="text-2xl font-bold text-white">Call Us</h2>
               <p className="text-white text-lg max-w-md text-center">
@@ -120,7 +118,7 @@ function ContactPage() {
               </p>
               <a
                 href="tel:+13616522470"
-                className="mt-2 px-6 py-2 bg-green-600 text-white rounded-full font-semibold hover:bg-green-700 transition-colors duration-200"
+                className="mt-2 px-6 py-2 bg-green-600 text-white rounded-full font-semibold hover:bg-green-700 transition-colors duration-300 hover:ring-2 hover:ring-white animate-"
               >
                 +1 (361) 652-2470
               </a>
